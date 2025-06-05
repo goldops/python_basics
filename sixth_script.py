b41f596659ec270d67e230c6e20d0c4459f8fb85
Blent User <user@blent>
2025-06-05 00:50:00 +0000

    adding sixth script

---
+++ sixth_script.py
@@ -0,0 +1,6 @@
+personnes = [
+    ["Jean","Dupont","Paris"],
+    ["Marie","Durant","Lyon"],
+    ["Roger","Moulin","Niort"]
+]
+print(f"{[personnes[1][0]]},{[personned[1][1]]} vit à {[personnes[1][2]]}")
