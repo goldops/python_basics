9686b8926e5981472f33fd968037e721f5d0d0d5
Blent User <user@blent>
2025-06-05 00:42:02 +0000

    fifth script

---
+++ fifth_script.py
@@ -0,0 +1,8 @@
+liste = [7, 5, 9, 4, 1, 3, 10, 5, 4, 7, 3, 2, 0, 1]
+print(f"Dernier élément de la liste: {liste[-1]}")
+print(f"3ème élément en partant de la fin: {liste[-3]}")
+
+print(f"3 premiers éléments de la liste: {liste[:3]}")
+print(f"Tout sauf les 3 derniers éléments de la liste: {liste[:-3]}")
+
+print(f"Imprimer les éléments entre la position 2 et 7 (exclus): {liste[2:7]}")
